c8fcc90d05330e757905a1c872d258fddb663d9c
Michael Hall~ <brkthru.consulting@gmail.com>
2026-02-19 14:23:28 +0800

    V113: Stabilized Windows Push

---
+++ deploy.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+cls
+echo ===========================================
+echo   BRKTHRU DIGITAL - WINDOWS DEPLOY V113
+echo ===========================================
+echo.
+echo [1/3] STAGING CHANGES...
+git add .
+echo.
+echo [2/3] COMMITTING...
+git commit -m "V113: Stabilized Windows Push"
+echo.
+echo [3/3] PUSHING TO GITHUB...
+git push origin master:main --force
+echo.
+echo ===========================================
+echo   DEPLOYMENT SUCCESSFUL
+echo ===========================================
+pause
