a91fe48dd2b8b561616f31b91be54de83bbfec88
Michael Hall~ <brkthru.consulting@gmail.com>
2026-02-24 07:23:23 +0800

    Email fix

--- get_base64.py
+++
@@ -1,6 +0,0 @@
-import base64
-
-with open("brkthru-logo2.png", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-    with open("logo_b64.txt", "w") as out_file:
-        out_file.write(encoded_string)
